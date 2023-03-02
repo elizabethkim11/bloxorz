64ce7784bd954eee684a46d8da3fa5be208785f0
Jennifer Kim <jenniferkim@s-128-97-180-57.resnet.ucla.edu>
2023-03-02 15:57:46 -0800

    commit before merge

--- bloxorz.js
+++ bloxorz.js
@@ -15,10 +15,23 @@ export class Bloxorz_Base extends Scene {
             metal: new Material(phong,
                 {ambient: .2, diffusivity: .8, specularity: .8, color: color(.9, .5, .9, 1)})
         }
+        this.up = false;
+        this.left = false;
+        this.down = false;
+        this.right = false;
     }
     make_control_panel() {
-        this.key_triggered_button("Move block upward", ["w"], function () {
-            this.up ^= 1;
+        this.key_triggered_button("Move block up", ['i'], function () {
+            this.up = true;
+        });
+        this.key_triggered_button("Move block left", ['j'], function () {
+            this.left = true;
+        });
+        this.key_triggered_button("Move block down", ['k'], function () {
+            this.down = true;
+        });
+        this.key_triggered_button("Move block right", ['l'], function () {
+            this.right = true;
         });
     }
     display(context, program_state) {
@@ -31,7 +44,8 @@ export class Bloxorz_Base extends Scene {
             // treated when projecting 3D points onto a plane.  The Mat4 functions perspective() and
             // orthographic() automatically generate valid matrices for one.  The input arguments of
             // perspective() are field of view, aspect ratio, and distances to the near plane and far plane.
-            program_state.set_camera(Mat4.translation(0, 0, -10));
+            program_state.set_camera(Mat4.translation(0, 0, -20));
+            //program_state.set_camera(Mat4.look_at(vec3(0, 10, 20), vec3(0, 0, 0), vec3(0, 1, 0)));
         }
         program_state.projection_transform = Mat4.perspective(
             Math.PI / 4, context.width / context.height, 1, 100);
@@ -40,18 +54,37 @@ export class Bloxorz_Base extends Scene {
         // the shader when coloring shapes.  See Light's class definition for inputs.
         const t = this.t = program_state.animation_time / 1000;
         const angle = Math.sin(t);
-        const light_position = Mat4.rotation(angle, 1, 0, 0).times(vec4(0, -1, 1, 0));
+        const light_position = vec4(0, 1, 1, 0);
         program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1000)];
     }
 }
 
 export class Bloxorz extends Bloxorz_Base {
+    draw_block(context, program_state, model_transform, color) {
+        const angle = Math.PI;
+        if (this.left) {
+            model_transform = model_transform.times(Mat4.rotation(angle, 0, 0, 1));
+            model_transform = model_transform.times(Mat4.scale(1, 1.5, 1));
+            this.shapes.block.draw(context, program_state, model_transform, this.materials.metal.override({color: color}));
+            console.log("WORKED");
+            console.log(model_transform);
+        }
+        else {
+            model_transform = model_transform.times(Mat4.scale(1, 1.5, 1));
+            this.shapes.block.draw(context, program_state, model_transform, this.materials.metal.override({color: color}));
+        }
+        return model_transform;
+    }
     display(context, program_state) {
         super.display(context, program_state);
-        const yellow = hex_color("#fdc03a")
+        const brown = hex_color("#7B3F00")
         let model_transform = Mat4.identity();
-        model_transform = model_transform.times(Mat4.translation(0, 0, 0));
-        this.shapes.block.draw(context, program_state, model_transform, this.materials.metal.override(yellow));
 
+        for (let i = 0; i < 2; i++) {
+            model_transform = this.draw_block(context, program_state, model_transform, brown);
+        }
+        if (this.left) {
+            this.left = false;
+        }
     }
 }
