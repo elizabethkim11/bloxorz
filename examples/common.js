c6b82aee4a5f35766d436e1a65bdde5d672195a1
mithilc <42623494+mithilc@users.noreply.github.com>
2023-03-02 16:18:52 -0800

    3x3 platform for base lvl 1

--- examples/common.js
+++ examples/common.js
@@ -154,6 +154,23 @@ const Cube = defs.Cube =
                 }
         }
     }
+const Tiles = defs.Tiles =
+    class Tiles extends Shape {
+        constructor() {
+            super("position", "normal", "texture_coord");
+            // Loop 3 times (for each axis), and inside loop twice (for opposing cube sides):
+            for (let i = 0; i < 3; i++)
+                for (let j = 0; j < 2; j++) {
+                    let square_transform = Mat4.scale(1, .1, 1).times(Mat4.rotation(i == 0 ? Math.PI / 2 : 0, 1, 0, 0))
+                        .times(Mat4.rotation(Math.PI * j - (i == 1 ? Math.PI / 2 : 0), 0, 1, 0))
+                        .times(Mat4.translation(0, 0, 1));
+                    // square_transform = square_transform.times(Mat4.scale(1, 1, 0));
+                    // Calling this function of a Square (or any Shape) copies it into the specified
+                    // Shape (this one) at the specified matrix offset (square_transform):
+                    Square.insert_transformed_copy_into(this, [], square_transform);
+                }
+        }
+    }
 
 
 const Subdivision_Sphere = defs.Subdivision_Sphere =
--- main-scene.js
+++ main-scene.js
@@ -8,6 +8,7 @@ import {Surfaces_Demo} from "./examples/surfaces-demo.js"
 import {Text_Demo} from "./examples/text-demo.js"
 import {Transforms_Sandbox} from "./examples/transforms-sandbox.js"
 import {Bloxorz} from "./bloxorz.js";
+import {Tile} from "./tile.js";
 // Pull these names into this module's scope for convenience:
 const {
     Vector, Vector3, vec, vec3, vec4, color, Matrix, Mat4, Light, Shape, Material, Shader, Texture, Scene,
@@ -33,13 +34,14 @@ Object.assign(defs,
             {Surfaces_Demo},
             {Text_Demo},
             {Transforms_Sandbox},
+            {Tile},
             {Bloxorz});
 
 // ******************** End extra step
 
 // (Can define Main_Scene's class here)
 
-const Main_Scene = Bloxorz;
+const Main_Scene = Tile;
 const Additional_Scenes = [];
 
 export {Main_Scene, Additional_Scenes, Canvas_Widget, Code_Widget, Text_Widget, defs}
